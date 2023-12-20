fd269ca2ce2f70a44cff601692f734468d7fe3c5
Jihoon Moon <moon34git.gmail.com>
2023-12-20 19:27:07 +0900

    12201927

--- site.config.js
+++ site.config.js
@@ -12,8 +12,8 @@ const CONFIG = {
   },
   projects: [
     {
-      name: `morethan-log`,
-      href: "https://github.com/morethanmin/morethan-log",
+      name: `TBD`,
+      href: "",
     },
   ],
   // blog setting (required)
